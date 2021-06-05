6673f67b3b76a3670e6cd7257e073555c32aa190
Sergio <sergiocherevko9999@gmail.com>
2021-06-05 19:49:56 +0300

    sekond commit

---
+++ js/script.js
@@ -0,0 +1,55 @@
+"use strict";
+
+// alert('Hello');
+
+// const result = confirm("Are you here?");
+// console.log(result);
+
+// const answer = +prompt ("Вам есть 18?", "18" );
+// console.log(answer + 5);
+
+// const answers = [];
+
+// answers[0] = prompt('Как ваше имя?', "" );
+// answers[1] = prompt('Как ваше фамилие?', "" );
+// answers[2] = prompt('Сколько вам лет?', "" );
+
+// // document.write(answers);
+
+// console.log(typeof(answers));
+// console.log(typeof(null));
+
+
+
+// let number = 4.6;
+
+// // console.log(-4/0)
+// console.log("String" * 9);
+
+// const persone = "Alex";
+
+// const bool = true;
+
+// // console.log(something);
+
+// let und;
+// console.log(und);
+
+// const obj = {
+// name: "John",
+// age: 25,
+// isMarried: false
+
+// };
+
+// console.log(obj.age);
+
+
+
+// const category = "toys";
+
+// console.log("https://someurl.com/" + category);
+
+const user = "Ivan";
+
+alert("Привет, ${user}");
